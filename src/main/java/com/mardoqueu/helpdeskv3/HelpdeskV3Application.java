fa27afbece6c0f99efb2dfe00c7dd95ca1578d5d
Mardoqueu Sousa <mdq767@gmail.com>
2023-03-11 22:03:52 -0300

    starting project

---
+++ src/main/java/com/mardoqueu/helpdeskv3/HelpdeskV3Application.java
@@ -0,0 +1,13 @@
+package com.mardoqueu.helpdeskv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpdeskV3Application.class, args);
+    }
+
+}
