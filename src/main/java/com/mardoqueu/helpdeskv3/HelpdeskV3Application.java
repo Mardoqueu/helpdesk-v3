1882de14e8afa46bb01075d6c20d764956488e44
Mardoqueu Sousa <mdq767@gmail.com>
2023-03-12 21:26:00 -0300

    test profile

--- src/main/java/com/mardoqueu/helpdeskv3/HelpdeskV3Application.java
+++ src/main/java/com/mardoqueu/helpdeskv3/HelpdeskV3Application.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelpdeskV3Application {
 
+
     public static void main(String[] args) {
         SpringApplication.run(HelpdeskV3Application.class, args);
     }
+
 
 }
